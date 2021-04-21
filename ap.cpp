6faa951e90861f3996fd18445c3e90ee363fb4f4
pooja9983 <69471390+pooja9983@users.noreply.github.com>
2021-04-21 20:07:17 +0530

    c++

---
+++ ap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int data[5];
+	cout <<" enter elements:";
+	
+	 for ( int i=1;i<5;++i)
+	     cin>>data[i];
+	 
+	 cout <<"you  entered: ";
+	 for (int i=1;i<5;++i)
+	    cout << endl << *(data+i);
+	    
+	    
+return 0;	    
+	
+}
+
+
+ 
